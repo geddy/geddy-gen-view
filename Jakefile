2790a223a5d2aa179d4180fb211985dd536d5e44
Ondrej Brinkel <info@anzui.de>
2014-07-29 21:19:09 +0200

    working generator, supporting custom view data

--- Jakefile
+++ Jakefile
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path')
+  , utilities = require('utilities')
   , geddyPath = path.normalize(path.join(require.resolve('geddy'), '../../'));
 
 // Load the basic Geddy toolkit
@@ -11,11 +12,97 @@ var cwd = process.cwd()
   , Adapter = require(path.join(geddyPath, 'lib/template/adapters')).Adapter
   , genDirname = __dirname;
 
+function flagSet(shortName, name) {
+  return process.argv.indexOf(shortName) !== -1 || process.argv.indexOf(name) !== -1;
+}
+
+var _writeTemplate = function (src, dest, data) {
+  var ext = path.extname(src)
+    , supported = ['.ejs', '.jade', '.mustache', '.swig', '.handlebars']
+
+  if (supported.indexOf(ext) == -1) {
+    fail('Unsuported template engine. Try one of these instead: ' + supported.join(', '));
+    return;
+  }
+
+  var text = fs.readFileSync(src, 'utf8').toString()
+    , adapter
+    , templContent;
+
+  // render with the correct adapter
+  adapter = new Adapter({engine: ext.substring(1), template: text});
+  templContent = adapter.render(data);
+
+  // Write file
+  fs.writeFileSync(dest, templContent, 'utf8');
+
+  console.log('[Added] ' + dest);
+};
 
 // Tasks
 task('default', function() {
-  // add your code here
-  console.log('This is the default task of this generator.\nIt does nothing yet.');
+  var self = this;
+  var t = jake.Task.create;
+  t.reenable();
+  t.invoke.apply(t, Array.prototype.slice.call(arguments));
+});
+
+task('create', function(name, template, data) {
+  if (!name) {
+    fail('Missing view name.');
+    return;
+  }
+
+  if (!template) {
+    fail('Missing view template.');
+    return;
+  }
+
+  var appPath = process.cwd();
+  var viewsDir = path.join(appPath, 'app', 'views');
+  if (!fs.existsSync(viewsDir) || !fs.statSync(viewsDir).isDirectory()) {
+    fail('You must run this generator from the root of your application.');
+    return;
+  }
+
+  // try to parse data
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data) || {};
+    } catch(error) {
+      data = {};
+    }
+  }
+  else if(typeof data !== 'object') {
+    data = {};
+  }
+
+  var force = flagSet('-f','--force');
+  var usePublic = flagSet('-p', '--public');
+
+  var viewPath = path.join(appPath, usePublic ? 'public' : 'app', 'views', name);
+  var viewDir = path.dirname(viewPath);
+
+  if (!fs.existsSync(template)) {
+    fail('Template file does not exists.');
+    return;
+  }
+
+  if (!force && fs.existsSync(viewPath)) {
+    fail('View already exists. Use -f to replace it.');
+    return;
+  }
+
+  // create destination dir if it does not exists
+  jake.mkdirP(viewDir);
+
+  // extend data
+  data.geddy = geddy;
+  data.viewFileName = path.basename(viewPath);
+  data.viewDirName = path.relative(appPath, viewDir);
+  data.viewIsPublic = usePublic;
+
+  _writeTemplate(template, viewPath, data);
 });
 
 task('help', function() {
@@ -25,4 +112,32 @@ task('help', function() {
       {encoding: 'utf8'}
     )
   );
+});
+
+testTask('Resource', ['clean', 'prepare-test-app'], function() {
+  this.testFiles.exclude('test/helpers/**');
+  this.testFiles.exclude('test/fixtures/**');
+  this.testFiles.exclude('test/template/**');
+  this.testFiles.exclude('test/geddy-test-app');
+  this.testFiles.exclude('test/tmp/**');
+  this.testFiles.include('test/**/*.js');
+});
+
+desc('Clears the test temp directory.');
+task('clean', function() {
+  console.log('Cleaning temp files ...');
+  var tmpDir = path.join(__dirname, 'test', 'tmp');
+  utilities.file.rmRf(tmpDir, {silent:true});
+  fs.mkdirSync(tmpDir);
+});
+
+desc('Copies the test app into the temp directory.');
+task('prepare-test-app', function() {
+  console.log('Preparing test app ...');
+  jake.cpR(
+    path.join(__dirname, 'test', 'geddy-test-app'),
+    path.join(__dirname, 'test', 'tmp'),
+    {silent: true}
+  );
+  console.log('Test app prepared.');
 });
---
+++ test/view.js
@@ -0,0 +1,124 @@
+var path = require('path')
+  , assert = require('assert')
+  , fs = require('fs')
+  , exec = require('child_process').exec
+  , utilities = require('utilities')
+  , tests;
+
+var testAppDir = path.join(__dirname, 'tmp', 'geddy-test-app');
+var viewsDir = path.join(testAppDir, 'app', 'views');
+var publicViewsDir = path.join(testAppDir, 'public', 'views');
+
+function createView(name, template, argv, cb)
+{
+  if (!argv) {
+    argv = []
+  }
+
+  if (typeof argv === 'function') {
+    cb = argv;
+    argv = [];
+  }
+
+  var p = exec(path.join(__dirname, 'helpers', 'exec.js') + ' ' + name + ' ' + template + ' ' + argv.join(' '), cb);
+  p.stdout.pipe(process.stdout);
+}
+
+tests = {
+  'beforeEach': function() {
+    // go to app root
+    process.chdir(path.join(__dirname, 'tmp', 'geddy-test-app'));
+  },
+  'Call outside of test app\'s root': function(next) {
+    process.chdir('./app');
+    createView('template.html', path.join(__dirname, 'template', 'template.html.ejs'), function(err, stdout, stderr) {
+      assert.equal(stderr.split('\n')[1], 'Error: You must run this generator from the root of your application.');
+      next();
+    });
+  },
+  'Create a view': function(next) {
+    createView('foo/first.html', path.join(__dirname, 'template', 'template.html.ejs'), function(err, stdout, stderr) {
+      if (err) {
+        console.log(err);
+        fail();
+        return;
+      }
+
+      var viewPath = path.join(viewsDir, 'foo', 'first.html');
+      assert.equal(fs.existsSync(viewPath), true);
+      assert.equal(fs.readFileSync(viewPath, 'utf8'), fs.readFileSync(path.join(__dirname, 'fixtures', 'template.html')));
+      next();
+    });
+  },
+  'Create a public view': function(next) {
+    createView('foo/second.html', path.join(__dirname, 'template', 'template.html.ejs'), ['-p'], function(err, stdout, stderr) {
+      if (err) {
+        console.log(err);
+        fail();
+        return;
+      }
+
+      var viewPath = path.join(publicViewsDir, 'foo', 'second.html');
+      assert.equal(fs.existsSync(viewPath), true);
+      assert.equal(fs.readFileSync(viewPath, 'utf8'), fs.readFileSync(path.join(__dirname, 'fixtures', 'public', 'template.html')));
+
+      createView('foo/third.html', path.join(__dirname, 'template', 'template.html.ejs'), ['--public'], function(err, stdout, stderr) {
+        if (err) {
+          console.log(err);
+          fail();
+          return;
+        }
+
+        var viewPath = path.join(publicViewsDir, 'foo', 'third.html');
+        assert.equal(fs.existsSync(viewPath), true);
+        assert.equal(fs.readFileSync(viewPath, 'utf8'), fs.readFileSync(path.join(__dirname, 'fixtures', 'public', 'template.html')));
+        next();
+      });
+    });
+  },
+  'Re-create the same view': function(next) {
+    createView('foo/first.html', path.join(__dirname, 'template', 'template.html.ejs'), function(err, stdout, stderr) {
+      assert.equal(stderr.split('\n')[1], 'Error: View already exists. Use -f to replace it.');
+      next();
+    });
+  },
+  'Overwrite the view': function(next) {
+    createView('foo/first.html', path.join(__dirname, 'template', 'template.html.ejs'), ['-f'], function(err, stdout, stderr) {
+      if (err) {
+        console.log(err);
+        fail();
+        return;
+      }
+
+      var viewPath = path.join(viewsDir, 'foo', 'first.html');
+      assert.equal(fs.existsSync(viewPath), true);
+      assert.equal(fs.readFileSync(viewPath, 'utf8'), fs.readFileSync(path.join(__dirname, 'fixtures', 'template.html')));
+
+
+      createView('foo/first.html', path.join(__dirname, 'template', 'template.html.ejs'), ['--force'], function (err, stdout, stderr) {
+        if (err) {
+          console.log(err);
+          fail();
+          return;
+        }
+
+        var viewPath = path.join(viewsDir, 'foo', 'first.html');
+        assert.equal(fs.existsSync(viewPath), true);
+        assert.equal(fs.readFileSync(viewPath, 'utf8'), fs.readFileSync(path.join(__dirname, 'fixtures', 'template.html')));
+        next();
+      });
+    });
+  },
+  'Create view programaticly with data': function()
+  {
+    var viewGen = require('../index.js');
+
+    viewGen.create('foo/data.html', path.join(__dirname, 'template', 'data.html.ejs'), { foo: 'bar', bar: 'baz'});
+
+    var viewPath = path.join(viewsDir, 'foo', 'data.html');
+    assert.equal(fs.existsSync(viewPath), true);
+    assert.equal(fs.readFileSync(viewPath, 'utf8'), fs.readFileSync(path.join(__dirname, 'fixtures', 'data.html')));
+  }
+};
+
+module.exports = tests;
